95d6f5cf8fa15cb95df876e032601f029733b16f
gayratn1st <gayratn1st@gmail.com>
2026-01-29 17:30:35 +0900

    feat: meta tags integrations on document private

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log(" DOCUMENT  -- PAGES ROUTER");
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en"> 
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/* SEO */}
+        <meta
+          name="keyword"
+          content={"nestar , nestar.uz, devex mern, mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. | " +
+            "대한민국 언제 어디서나 부동산을 사고팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -12,4 +30,7 @@ export default function Document() {
     </Html>
   );
 }
+
 // SERVICE side rendering
+// SEO - searching engnine  optimization , Metadatalarni integratsiyasi uchun
+// wee neeed Metadatalar to share our website's link / Description and other info
