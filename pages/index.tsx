831ddd992e74b312f2bb4eed95e1b984e9d7aa5d
gayratn1st <gayratn1st@gmail.com>
2026-02-05 17:43:03 +0900

    feat: Apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,32 @@ import { NextPage } from "next";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { GET_PROPERTIES } from "@/apollo/user/query";
+import { useQuery } from "@apollo/client";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+
+  console.log("getPropertiesData =>", getPropertiesData);
+
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
   } else {
