80c76eab06e491ecb5281f085f83f09334e708dd
gayratn1st <gayratn1st@gmail.com>
2026-02-05 17:14:49 +0900

    feat: develop MOBILE version

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -2,48 +2,73 @@ import { Stack } from "@mui/material";
 import Head from "next/head";
 import Top from "../Top";
 import Footer from "../Footer";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 
 const withLayoutBasic = (Component: any) => {
   return (props: any) => {
-    return (
-      <>
-        <Head>
-          <title>Nestar</title>
-        </Head>
+    const device = useDeviceDetect();
+    if (device === "mobile") {
+      return (
+        <>
+          <Head>
+            <title>Nestar</title>
+          </Head>
+          <Stack id="mobile-wrap">
+            <Stack id="top">
+              <Top />
+            </Stack>
 
-        <Stack id="pc-wrap">
-          {/* TOP NAVBAR */}
-          <Stack id="top">
-            <Top />
-          </Stack>
+            <Stack id="main">
+              <Component {...props} />
+            </Stack>
 
-          {/* HEADER BASIC */}
-          <Stack
-            className="header-basic"
-            style={{
-              backgroundImage: "url(/img/banner/properties.png)",
-              backgroundSize: "cover",
-              boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
-            }}
-          >
-            <Stack className="container">
-              <strong> Search</strong>
-              <span>We are glad to see you again!</span>
+            <Stack id="footer">
+              <Footer />
             </Stack>
           </Stack>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Head>
+            <title>Nestar</title>
+          </Head>
 
-          {/* PAGE CONTENT */}
-          <Stack id="main">
-            <Component {...props} />
-          </Stack>
+          <Stack id="pc-wrap">
+            {/* TOP NAVBAR */}
+            <Stack id="top">
+              <Top />
+            </Stack>
 
-          {/* FOOTER */}
-          <Stack id="footer">
-            <Footer />
+            {/* HEADER BASIC */}
+            <Stack
+              className="header-basic"
+              style={{
+                backgroundImage: "url(/img/banner/properties.png)",
+                backgroundSize: "cover",
+                boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
+              }}
+            >
+              <Stack className="container">
+                <strong> Search</strong>
+                <span>We are glad to see you again!</span>
+              </Stack>
+            </Stack>
+
+            {/* PAGE CONTENT */}
+            <Stack id="main">
+              <Component {...props} />
+            </Stack>
+
+            {/* FOOTER */}
+            <Stack id="footer">
+              <Footer />
+            </Stack>
           </Stack>
-        </Stack>
-      </>
-    );
+        </>
+      );
+    }
   };
 };
 
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -3,14 +3,18 @@ import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 const Community: NextPage = () => {
-  const [title, setTitle] = useState<string>("hello");
-
-  return (
-    <div style={{ margin: "20px 0" }}>
-      <Stack className="container">COMMUNITY</Stack>
-    </div>
-  );
+  const device = useDeviceDetect();
+  if (device === "mobile") {
+    return <Stack>COMMUNITY MOBILE</Stack>;
+  } else {
+    return (
+      <div style={{ margin: "20px 0" }}>
+        <Stack className="container">COMMUNITY</Stack>
+      </div>
+    );
+  }
 };
 
 export default withLayoutBasic(Community);
